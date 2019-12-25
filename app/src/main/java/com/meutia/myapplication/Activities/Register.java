80eef25ec9fb2a60f24eae8c26ba11368b90e7ca
MeutiaKhanandiya <Khanandyam@gmail.com>
2019-12-25 23:48:31 +0700

    Membuat Login Dan Register yang terkoneksi firebase

---
+++ app/src/main/java/com/meutia/myapplication/Activities/Register.java
@@ -0,0 +1,83 @@
+package com.meutia.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.meutia.myapplication.R;
+
+import java.security.Security;
+
+public class Register extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    EditText etUsername, etNama, etPassword, etAlamat, etEmail;
+    Button regbtn;
+    DatabaseReference databaseReference;
+
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        etUsername = findViewById(R.id.edit_username);
+        etNama = findViewById(R.id.edt_name);
+        etPassword = findViewById(R.id.edt_password);
+        etAlamat = findViewById(R.id.edt_alamat);
+        etEmail = findViewById(R.id.edt_email);
+        regbtn = findViewById(R.id.btn_req);
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addUser();
+
+
+            }
+
+            private void addUser() {
+                final String username = etUsername.getText().toString();
+                final String nama = etNama.getText().toString();
+                final String password = etPassword.getText().toString();
+                final String alamat = etAlamat.getText().toString();
+                final String email = etEmail.getText().toString();
+
+                if (username.isEmpty() || nama.isEmpty() || password.isEmpty() || alamat.isEmpty() || email.isEmpty()){
+                    showMessage("Mohon isi semua kolom ");
+                }else {
+                    User user= new User(username,nama,password,email,alamat);
+                    databaseReference.child(username).child("username").setValue(username.toString());
+                    databaseReference.child(username).child("nama").setValue(nama.toString());
+                    databaseReference.child(username).child("email").setValue(email.toString());
+                    databaseReference.child(username).child("alamat").setValue(alamat.toString());
+                    try {
+                        databaseReference.child(username).child("password").setValue(Security.encrypt(password));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getApplication(),"Success",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplication(),Login.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+}

9075d521b41fddb591323363b1306bb3909df999
MeutiaKhanandiya <Khanandyam@gmail.com>
2019-12-25 23:21:04 +0700

    melakukan edit di MainActivity

--- app/src/main/java/com/meutia/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/meutia/myapplication/Activities/MainActivity.java
@@ -1,16 +1,55 @@
 package com.meutia.myapplication.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.meutia.myapplication.Fragments.ItemFragment;
+import com.meutia.myapplication.Fragments.OrderFragment;
 import com.meutia.myapplication.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new OrderFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_item:
+                fragment = new ItemFragment();
+                break;
+            case R.id.action_order:
+                fragment = new OrderFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
